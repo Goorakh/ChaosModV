22708db2031bdb14e43670af17b66256261b061c
petter.ohlen@gmail.com <petter.ohlen@gmail.com>
2021-09-12 13:11:38 +0200

    Added chaos multiplier to Ped Rain

--- ChaosMod/Effects/db/Misc/MiscPedRain.cpp
+++ ChaosMod/Effects/db/Misc/MiscPedRain.cpp
@@ -26,7 +26,7 @@ static void OnTick()
 	static DWORD64 lastTick = 0;
 	DWORD64 curTick = GET_GAME_TIMER();
 
-	if (curTick > lastTick + 300)
+	if (curTick > lastTick + (300 / g_MetaInfo.m_fChaosMultiplier))
 	{
 		lastTick = curTick;
 
