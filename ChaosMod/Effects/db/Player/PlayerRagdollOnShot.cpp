b0d056ddea58de3a786ff8becbcf49154b9b6a3f
Petter Öhlén <petter.ohlen@gmail.com>
2021-09-19 11:52:27 +0200

    Make player able to ragdoll during Player Ragdolls When Shot effect

--- ChaosMod/Effects/db/Player/PlayerRagdollOnShot.cpp
+++ ChaosMod/Effects/db/Player/PlayerRagdollOnShot.cpp
@@ -16,6 +16,7 @@ static void OnTick()
 				CLEAR_PED_TASKS_IMMEDIATELY(playerPed);
 			}
 
+			SET_PED_CAN_RAGDOLL(playerPed, true);
 			SET_PED_TO_RAGDOLL(playerPed, 500 * g_MetaInfo.m_fChaosMultiplier, 1000 * g_MetaInfo.m_fChaosMultiplier, 0, true, true, false);
 
 			CREATE_NM_MESSAGE(true, 0);
