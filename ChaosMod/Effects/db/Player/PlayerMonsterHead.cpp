12795fea4f00583d1897c45cd7bfa3baef48e6a0
Petter Öhlén <petter.ohlen@gmail.com>
2021-09-22 15:48:29 +0200

    Make Monster Head timed

--- ChaosMod/Effects/db/Player/PlayerMonsterHead.cpp
+++ ChaosMod/Effects/db/Player/PlayerMonsterHead.cpp
@@ -4,24 +4,45 @@
 
 #include <stdafx.h>
 
-static const Hash monsterHash = GET_HASH_KEY("sum_prop_ac_monstermask_01a");
+static Object monsterHead = 0;
 
-static void OnStart()
+static void OnTick()
 {
-	LoadModel(monsterHash);
-
 	Ped player = PLAYER_PED_ID();
-	int bone = GET_PED_BONE_INDEX(player, 0x60F2);
-	Vector3 playerPos = GET_ENTITY_COORDS(player, false);
 
-	Object monsterHead = CREATE_OBJECT(monsterHash, playerPos.x, playerPos.y + 2, playerPos.z, true, false, false);
-	ATTACH_ENTITY_TO_ENTITY(monsterHead, player, bone, 0.36, 0.1, -0.01, 180, -90, 0, true, false, false, false, 0, true);
-	SET_ENTITY_AS_NO_LONGER_NEEDED(&monsterHead);
+	if (!monsterHead)
+	{
+		static const Hash monsterHash = GET_HASH_KEY("sum_prop_ac_monstermask_01a");
+		LoadModel(monsterHash);
+
+		int bone = GET_PED_BONE_INDEX(player, 0x60F2);
+		Vector3 playerPos = GET_ENTITY_COORDS(player, false);
+
+		monsterHead = CREATE_OBJECT(monsterHash, playerPos.x, playerPos.y + 2, playerPos.z, true, false, false);
+		ATTACH_ENTITY_TO_ENTITY(monsterHead, player, bone, 0.36, 0.1, -0.01, 180, -90, 0, true, false, false, false, 0, true);
+		SET_MODEL_AS_NO_LONGER_NEEDED(monsterHash);
+	}
+	else if (!IS_ENTITY_ATTACHED_TO_ENTITY(monsterHead, player))
+	{
+		int bone = GET_PED_BONE_INDEX(player, 0x60F2);
+		ATTACH_ENTITY_TO_ENTITY(monsterHead, player, bone, 0.36, 0.1, -0.01, 180, -90, 0, true, false, false, false, 0, true);
+	}
 }
 
-static RegisterEffect registerEffect(EFFECT_PLAYER_MONSTER_HEAD, OnStart, EffectInfo
+static void OnStop()
+{
+	if (monsterHead)
+	{
+		DELETE_ENTITY(&monsterHead);
+	}
+
+	monsterHead = 0;
+}
+
+static RegisterEffect registerEffect(EFFECT_PLAYER_MONSTER_HEAD, nullptr, OnStop, OnTick, EffectInfo
 	{
 		.Name = "Monster Head",
-		.Id = "player_monster_head"
+		.Id = "player_monster_head",
+		.IsTimed = true
 	}
 );
