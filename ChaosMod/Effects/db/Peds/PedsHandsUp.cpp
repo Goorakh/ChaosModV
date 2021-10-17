a47a1ababde4e50308a2993f31a57f770ffaa2a3
Petter Öhlén <petter.ohlen@gmail.com>
2021-10-17 21:07:08 +0200

    Remove excluding police and NOOSE from "Hands Up\!"

--- ChaosMod/Effects/db/Peds/PedsHandsUp.cpp
+++ ChaosMod/Effects/db/Peds/PedsHandsUp.cpp
@@ -8,9 +8,7 @@ static void OnStart()
 {
 	for (Ped ped : GetAllPeds())
 	{
-		int pedType = GET_PED_TYPE(ped);
-
-		if (pedType != 6 && pedType != 27 && !IS_PED_DEAD_OR_DYING(ped, true))	// 6: cop, 27: NOOSE
+		if (!IS_PED_DEAD_OR_DYING(ped, true))
 		{
 			TASK_HANDS_UP(ped, 5000 * g_MetaInfo.m_fChaosMultiplier, 0, -1, true);
 			SET_PED_DROPS_WEAPON(ped);
