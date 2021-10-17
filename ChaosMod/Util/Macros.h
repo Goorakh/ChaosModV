d064bc6bdd12f4ff697c0f162016705876e035df
Petter Öhlén <petter.ohlen@gmail.com>
2021-10-17 21:05:42 +0200

    Minor variable changes

--- ChaosMod/Util/Macros.h
+++ ChaosMod/Util/Macros.h
@@ -5,3 +5,9 @@
 #define _INSTR_NOP 0x90
 #define _INSTR_RET 0xC3
 #define _INSTR_DEC 0x48
+
+// Evaluates to 0 at g_MetaInfo.m_fChaosMultiplier = 1, evaluates to x at g_MetaInfo.m_fChaosMultiplier = infinity
+#define approach_rt(x, rate) (((x) * (1.f - (1.f / ((g_MetaInfo.m_fChaosMultiplier * (rate)) - ((rate) - 1.f))))))
+
+// Evaluates to 0 at g_MetaInfo.m_fChaosMultiplier = 1, evaluates to x at g_MetaInfo.m_fChaosMultiplier = infinity
+#define approach(x) approach_rt(x, 1.f)
