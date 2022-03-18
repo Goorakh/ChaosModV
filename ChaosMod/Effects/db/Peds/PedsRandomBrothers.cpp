649e569adcdcdf77b26e07192339079ad1d911aa
petter.ohlen@gmail.com <petter.ohlen@gmail.com>
2022-03-18 22:10:53 +0100

    Update PedsRandomBrothers

--- ChaosMod/Effects/db/Peds/PedsRandomBrothers.cpp
+++ ChaosMod/Effects/db/Peds/PedsRandomBrothers.cpp
@@ -21,7 +21,13 @@ static std::vector<Hash> GetAvailableVehModels()
 static void OnStart()
 {
 	static const std::vector<Hash> vehModels = GetAvailableVehModels();
-	SetSurroundingPedsInVehicles(vehModels[g_Random.GetRandomInt(0, vehModels.size() - 1)], -1, 30.f);
+	Hash chosenModel = vehModels[g_Random.GetRandomInt(0, vehModels.size() - 1)];
+	SetSurroundingPedsInVehicles(chosenModel, -1, 30.f);
+
+	WAIT(0);
+
+	std::string vehName = _GET_LABEL_TEXT(GET_DISPLAY_NAME_FROM_VEHICLE_MODEL(chosenModel));
+	g_pEffectDispatcher->OverrideEffectName(EFFECT_PEDS_RANDOMBROTHERS, vehName + " Brothers");
 }
 
 static RegisterEffect registerEffect(EFFECT_PEDS_RANDOMBROTHERS, OnStart, EffectInfo
